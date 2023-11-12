dafce51f9ccf8438b7dc7b07b1f7ca1672fd1240
avazrod <avazrod33@gmail.com>
2023-11-12 23:29:41 +0100

    Se comienzan a implentar las pruebas #1

---
+++ Tests/IntegersCalculator.java
@@ -0,0 +1,47 @@
+public class IntegersCalculator {
+
+    /**
+     * Returns a + b
+     * 
+     * @param a
+     * @param b
+     * @return a + b
+     */
+    public int add (int a, int b) {
+        return (a + b);
+    }
+    
+    /**
+     * Returns a - b
+     * 
+     * @param a
+     * @param b
+     * @return a - b
+     */
+    public int substraction (int a, int b){
+        return (a - b);
+    }
+    
+    /**
+     * Returns a * b
+     * 
+     * @param a
+     * @param b
+     * @return a * b
+     */
+    public int multiplication (int a, int b){
+        return (a * b);
+    }
+    
+    /**
+     * Returns a / b
+     * 
+     * @param a
+     * @param b
+     * @return a / b
+     */
+    public int division (int a, int b){
+        return (a / b);
+    }
+    
+}
